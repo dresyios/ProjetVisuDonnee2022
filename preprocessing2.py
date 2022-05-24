9e4184ebda96335b69dbb296023a4e41a6b39f4e
mjondeau <jondeaumaxime@gmail.com>
2022-05-24 16:31:00 +0200

    dataset2.csv

--- preprocessing2.py
+++ preprocessing2.py
@@ -6,20 +6,26 @@ data= pd.read_csv("CSVs/salesMerged(96).csv", usecols= ['Country','Year','Publis
 
 newData=[]
 
-for year in range(1980,1996):
-    countries=[]
-    print("Year: ", year)
+for year in range(1980,1982):
+    publishers={}
     for index, row in data.iterrows():
-        if (data.loc[index, 'Country'] not in countries) and (data.loc[index, 'Year']==year):
-            countries.append(data.loc[index, 'Country'])
+        if (data.loc[index, 'Publisher'] not in publishers.keys() and (data.loc[index, 'Year']==year)):
+            #print("Publisher: ", data.loc[index, 'Publisher'])
+            #print("Country: ",data.loc[index, 'Country'])
+            publishers[data.loc[index, 'Publisher']]=(data.loc[index, 'Country'])
 
-    print("Pays ", year, " :", countries)
-    
+    print("Publishers ", year, " :", publishers)
+
+
     currentArray=[]
-    for index, x in enumerate(countries):
-        print("Index: ",index)
-        print("Country: ",x)
-        currentArray.append([year, x, locations[x][0], locations[x][1], round(data.loc[(data['Year'] == year) & (data['Country'] == x), 'Total_Sales'].sum(),2)])
+    for key in publishers:
+        #print("Publisher: ",key)
+        #print("Country: ",publishers[key])
+        currentArray.append([year, key, publishers[key], round(data.loc[(data['Year'] == year) & (data['Country'] == publishers[key]) & (data['Publisher']== key), 'Total_Sales'].sum(),2)])
+    print("Current array: ", currentArray)
+    
+    #TRIER CURRENT ARRAY LA
+    
 
     newData.append(currentArray)
 
@@ -30,10 +36,10 @@ for item in newData:
     for subitem in item:
         flat_newData.append(subitem)
 
-print("flat: ", flat_newData)
+print("Flat array: ", flat_newData)
 
 dataNP = np.array(flat_newData)
 print("dataNP: ", dataNP)
 
-df = pd.DataFrame(dataNP, columns=['Year','Country','LocX','LocY','Sales'])
+df = pd.DataFrame(dataNP, columns=['Year','Publisher','Country','Sales'])
 df.to_csv('dataset2.csv', index=False)
