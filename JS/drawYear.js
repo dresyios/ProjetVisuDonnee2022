10a2824f694a3f5e75105dfb1a08743f6ceff8fd
mjondeau <jondeaumaxime@gmail.com>
2022-05-24 14:21:36 +0200

    Pushed new main2
    Added scripts in a JS folder, finalised new main2.html, created preprocessing2.py

---
+++ JS/drawYear.js
@@ -0,0 +1,77 @@
+function drawYear(year) {
+
+    // lier les données
+    d3.csv("dataset.csv", function(error, data) {
+        if (error) {
+            throw error;
+        }
+
+        console.log("Hello")
+        let dataYear = data.filter(function(d)
+        { 
+        if(d["Year"] == year) { 
+            return d;
+        } 
+        })
+
+        console.log(dataYear)
+
+        let totalYear = d3.sum(dataYear, d => d.Sales)
+        console.log(totalYear)
+    
+
+
+        // FONCTIONNALITE : mettre le cercle en évidence et afficher des infos quand on passe la souris dessus
+        let Tooltip = d3.select("body")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px")
+
+        // les fonctions pour faire cela
+        let mouseover = function(d) {
+            Tooltip
+            .style("opacity", 1)
+            d3.select(this)
+            .style("stroke", "black")
+            .style("opacity", 1)
+        }
+        let mousemove = function(d) {
+            Tooltip
+            .html("Pays :" + d.Country + "<br> Ventes totales sur l'année :" + d.Sales) //ne fonctionne pas pour l'instant
+            .style("left", (d3.mouse(this)[0]+70) + "px")
+            .style("top", (d3.mouse(this)[1]) + "px")
+        }
+        let mouseleave = function(d) {
+            Tooltip
+            .style("opacity", 0)
+            d3.select(this)
+            .style("stroke", "none")
+            .style("opacity", 0.8)
+        }
+
+        // création du canevas
+        canevas1.selectAll('circle')
+        .data(dataYear)
+        .enter()
+        .append('circle')
+            .attr('id', 'mouseovercercle')
+            .attr('cx', (d) => d.LocX)
+            .attr('cy', (d) => d.LocY)
+            .attr('r',  (d) => Math.sqrt((d.Sales*100)/totalYear)*5)
+            .attr('opacity', 0.7)
+            .attr("id", (d) => d.Country)
+            .style('fill', 'red')
+            .style("stroke-width", 4)
+            .style("stroke", "none")
+            .style("opacity", 0.8)
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave)
+                        
+        })
+}
---
+++ preprocessing2.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import math
+
+data= pd.read_csv("CSVs/salesMerged(96).csv", usecols= ['Country','Year','Publisher','Total_Sales'])
+
+newData=[]
+
+for year in range(1980,1996):
+    countries=[]
+    print("Year: ", year)
+    for index, row in data.iterrows():
+        if (data.loc[index, 'Country'] not in countries) and (data.loc[index, 'Year']==year):
+            countries.append(data.loc[index, 'Country'])
+
+    print("Pays ", year, " :", countries)
+    
+    currentArray=[]
+    for index, x in enumerate(countries):
+        print("Index: ",index)
+        print("Country: ",x)
+        currentArray.append([year, x, locations[x][0], locations[x][1], round(data.loc[(data['Year'] == year) & (data['Country'] == x), 'Total_Sales'].sum(),2)])
+
+    newData.append(currentArray)
+
+print('Array :', newData)
+
+flat_newData = []
+for item in newData:
+    for subitem in item:
+        flat_newData.append(subitem)
+
+print("flat: ", flat_newData)
+
+dataNP = np.array(flat_newData)
+print("dataNP: ", dataNP)
+
+df = pd.DataFrame(dataNP, columns=['Year','Country','LocX','LocY','Sales'])
+df.to_csv('dataset2.csv', index=False)
