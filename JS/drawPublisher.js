aaeec3ae4c1e9a5f23e8ef4339f435fd6e45f7cf
RiccHEX <100705313+RiccHEX@users.noreply.github.com>
2022-05-25 13:58:24 +0200

    Ajout : Les piecharts se retirent lorsque la souris quitte le cercle

--- JS/drawPublisher.js
+++ JS/drawPublisher.js
@@ -1,63 +1,63 @@
 function drawPublisher(year, country) {
 
 
-        var svg = d3.select("svg"),
-            width = 300,
-            height = 300,
-            radius = Math.min(width, height) / 2;
+    var svg = d3.select("svg"),
+        width = 300,
+        height = 300,
+        radius = Math.min(width, height) / 2;
 
-        //à chaque appel de la fonction je supprime la variable g, qui représente le piechart, pour éviter que les nouveaux piecharts se superposent
-        d3.select("#mypiechart").remove();
+    //à chaque appel de la fonction je supprime la variable g, qui représente le piechart, pour éviter que les nouveaux piecharts se superposent
+    d3.select("#mypiechart").remove();
 
+    
+    var g = svg.append("g")
+               .attr("transform", "translate(" + svgWidth / 2 + "," + ((svgHeight / 2)+100) + ")")
+               .attr("id", "mypiechart");
+
+    var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c', '#ad783e']);
+
+    var pie = d3.pie().value(function(d) { 
+        return d.Sales; 
+    }).sort(null);
+
+    var path = d3.arc()
+             .outerRadius(radius - 10)
+             .innerRadius(0);
+
+    var label = d3.arc()
+              .outerRadius(radius)
+              .innerRadius(radius - 80);
+
+    
+    d3.csv("dataset2.csv", function(error, data) {
+                if (error) {
+                    throw error;
+                }
+
+        let dataYear = data.filter(function(d)
+        { 
+            if(d["Year"] == year && d["Country"] == country) { 
+            return d;
+            } 
+        })
+        console.log(dataYear)
+
+        var arc = g.selectAll(".arc")
+                   .data(pie(dataYear))
+                   .enter().append("g")
+                   .attr("class", "arc")
+        console.log(data.Publisher)
+        arc.append("path")
+           .attr("d", path)
+           .attr("fill", function(d) { return color(d.data.Publisher); }) 
+           .attr("opacity", 0.8);       
         
-        var g = svg.append("g")
-                   .attr("transform", "translate(" + svgWidth / 2 + "," + ((svgHeight / 2)+100) + ")")
-                   .attr("id", "mypiechart");
-
-        var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c', '#ad783e']);
-
-        var pie = d3.pie().value(function(d) { 
-            return d.Sales; 
-        }).sort(null);
-
-        var path = d3.arc()
-                 .outerRadius(radius - 10)
-                 .innerRadius(0);
-
-        var label = d3.arc()
-                  .outerRadius(radius)
-                  .innerRadius(radius - 80);
-
-        
-        d3.csv("dataset2.csv", function(error, data) {
-                    if (error) {
-                        throw error;
-                    }
-
-            let dataYear = data.filter(function(d)
-            { 
-                if(d["Year"] == year && d["Country"] == country) { 
-                return d;
-                } 
+           arc.append("text")
+           .attr("transform", function(d) { 
+                    return "translate(" + label.centroid(d) + ")"; 
             })
-            console.log(dataYear)
-
-            var arc = g.selectAll(".arc")
-                       .data(pie(dataYear))
-                       .enter().append("g")
-                       .attr("class", "arc")
-            console.log(data.Publisher)
-            arc.append("path")
-               .attr("d", path)
-               .attr("fill", function(d) { return color(d.data.Publisher); }) 
-               .attr("opacity", 0.8);       
-            
-               arc.append("text")
-               .attr("transform", function(d) { 
-                        return "translate(" + label.centroid(d) + ")"; 
-                })
-               .text(function(d) { return d.data.Publisher; });
-            });
+           .text(function(d) { return d.data.Publisher; });
+        });
 
 
 }
