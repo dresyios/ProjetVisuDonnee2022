12ecf229813c5d6dbf2438b3e986dd216d57f489
mjondeau <jondeaumaxime@gmail.com>
2022-05-31 12:24:30 +0200

    Created dataset3.csv

--- JS/drawPublisher.js
+++ JS/drawPublisher.js
@@ -54,24 +54,23 @@ function drawPublisher(year, country) {
         var arc = g.selectAll(".arc")
                    .data(pie(dataYear))
                    .enter().append("g")
-                   .attr("class", "arc")
+                   .attr("class", "arc");
 
         console.log(data.Publisher)
         arc.append("path")
            .attr("d", path)
            .attr("fill", function(d) { return color(d.data.Publisher); })
-           .attr("opacity", 0.8);
-        
+           .attr("opacity", 0.8)
+           .attr("id", "path");
+
+
            arc.append("text")
            .attr("transform", function(d) { 
                     return "translate(" + label.centroid(d) + ")" +
                     "rotate(" + getAngle(d) + ")"; }) // pour améliorer la lisibilité (certains sont à l'envers pas contre, je chercher encore la solution)
             .attr("dy", 5) 
             .style("text-anchor", "start")
-            .text(function(d) { return d.data.Publisher; });
-        
-        //label pour les valeurs, à voir si ensuite on décide de les afficher différemment
-        console.log(data.Sales)     
+            .text(function(d) { return d.data.Publisher; }); 
            
             arc.append("text")
             .attr("transform", function(d) { 
--- preprocessing2.py
+++ preprocessing2.py
@@ -2,18 +2,19 @@ import pandas as pd
 import numpy as np
 import math
 
-data= pd.read_csv("CSVs/salesMerged(96).csv", usecols= ['Country','Year','Publisher','Total_Sales'], keep_default_na=False)
+data= pd.read_csv("CSVs/salesMerged(96).csv", usecols= ['Name','Country','Year','Publisher','Total_Sales'], keep_default_na=False)
 
 newData=[]
+newDataGames=[]
 
-for year in range(1980,1997):
+for year in range(1980,1996):
     #Créer dictionnaire éditeurs:pays
     publishers={}
     for index, row in data.iterrows():
         if (data.loc[index, 'Publisher'] not in publishers.keys() and (data.loc[index, 'Year']==year) and data.loc[index, 'Country']!=''):
             publishers[data.loc[index, 'Publisher']]=(data.loc[index, 'Country'])
 
-    print("Publishers ", year, " :", publishers)
+    #print("Publishers ", year, " :", publishers)
 
     #Créer une liste par année contenenant des listes pour chaque éditeur et pays
     currentArray=[]
@@ -37,7 +38,6 @@ for year in range(1980,1997):
         key = sub[2]
         if key not in d: d[key] = []
         d[key].append(sub)
-
     newArraySorted=[]
 
     for pays in d.keys():
@@ -48,22 +48,53 @@ for year in range(1980,1997):
             del d[pays][5:]
             d[pays].append([year, "Autres", pays, sommePlus5])
         newArraySorted.append(d[pays])
+
+    #Enlever une dimension
+    flatNewArraySorted = []
+    for item in newArraySorted:
+        for subitem in item:
+            flatNewArraySorted.append(subitem)
+
+    #Pour chaque éditeur, parcourir les lignes de la base de données pour créer un array contenent une ligne par jeux faisant partie du top 3 d'un éditeur
+    yearGamesArray = []
+    for publisher in flatNewArraySorted:
+        yearGamesPublisherArray=[]
+        i=0
+        for index, row in data.iterrows():
+            if ((data.loc[index, 'Year']==year) and (data.loc[index, 'Publisher']==publisher[1]) and i<3):
+                yearGamesPublisherArray.append([year, data.loc[index, 'Name'], publisher[1], publisher[2], data.loc[index, 'Total_Sales']])
+                i+=1
+        #print("Year game publisher array: ", yearGamesPublisherArray)
+        
+        yearGamesArray.append(yearGamesPublisherArray)
+        
     
-    print("New array sorted: ", newArraySorted)
+    flat_yearGamesArray = []
+    for item in yearGamesArray:
+        for subitem in item:
+            flat_yearGamesArray.append(subitem)
+    newDataGames.append(flat_yearGamesArray)
+    newData.append(flatNewArraySorted)
 
-    newData.append(newArraySorted)
+#Réduire dimension de games data
+print("New data games:", newDataGames)
+flat_newDataGames = []
+for item in newDataGames:
+    for subitem in item:
+        flat_newDataGames.append(subitem)
+print("New data games flat:", flat_newDataGames)
 
+#Réduire dimension de publisher data
 flat_newData = []
 for item in newData:
     for subitem in item:
         flat_newData.append(subitem)
 
-flat_newData2 = []
-for item in flat_newData:
-    for subitem in item:
-        flat_newData2.append(subitem)
-
-dataNP = np.array(flat_newData2)
+dataNP = np.array(flat_newData)
+dataGamesNP = np.array(flat_newDataGames)
 
 df = pd.DataFrame(dataNP, columns=['Year','Publisher','Country','Sales'])
 df.to_csv('dataset2.csv', index=False)
+
+dfGames = pd.DataFrame(dataGamesNP, columns=['Year','Names', 'Publisher','Country','Sales'])
+dfGames.to_csv('dataset3.csv', index=False)
